463c5b8b09a032d38faa45a492e219661a1ea64c
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-11-14 10:27:50 +0000

    Added fog of war

--- src/systems/FogOfWarSystem.gd
+++ src/systems/FogOfWarSystem.gd
@@ -2,6 +2,31 @@ extends System
 
 func on_process_entity(entity : Entity, delta: float):
 	#var c = ECS.entity_get_component(entity.id, "destinationcomponent")
-
+	check_if_visible(entity)
 	pass
+
+
+func check_if_visible(enemy):
+	var main = get_tree().get_root().get_node("Main")
+	var space_rid = main.get_world_2d().space
+	var space_state = Physics2DServer.space_get_direct_state(space_rid)
 	
+	enemy.get_node("Sprite").visible = false
+	for unit in main.units.values():
+		var result = check_can_see(enemy, unit, main, space_state)
+		if result:
+			enemy.get_node("Sprite").visible = true
+			break;
+		
+		
+func check_can_see(enemy, unit, main, space_state):
+	var result = space_state.intersect_ray(unit.position, enemy.position, [enemy])
+	return result.size() == 0
+		#var idx = unit.get_node("CollisionShape2D")
+		#var parent = result.collider.get_owner()
+		#if (result.collider == idx):
+		#enemy.get_node("Sprite").visible = false
+		#return
+		
+	#enemy.get_node("Sprite").visible = true
+		
